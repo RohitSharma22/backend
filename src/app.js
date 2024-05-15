748dc70a6d4c59a1c3f61e55905a071db9eaceef
RohitSharma22 <rs382706@gmail.com>
2024-05-15 17:32:32 +0530

    "changes made on 15 May"

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
-import cors from "cors"
-import express from "express"
+import cors from "cors";
+import express from "express";
 
+const app = express();
 
-const app = express()
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+//routes import
 
-export { app }
+import userRouter from "./routes/user.routes.js";
+
+//router decleration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
+export { app };
